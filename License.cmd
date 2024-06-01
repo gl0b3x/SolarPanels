f8236258044152a159930f9ff05adc3e15520c8c
Ivan Nikitin <67000098+gl0b3x@users.noreply.github.com>
2024-06-01 17:57:00 +0200

    Create License.cmd

---
+++ License.cmd
@@ -0,0 +1,27 @@
+# LICENSE
+
+## Restrictions on Use
+
+This software and its associated documentation files (the "Software") are prohibited from being used, copied, modified, merged, published, distributed, sublicensed, or sold. The following restrictions apply to the use of the Software:
+
+1. No Commercial Use: You may not use the Software for commercial purposes.
+2. No Distribution: You may not distribute the Software in any form.
+3. No Modification: You may not modify, adapt, or create derivative works of the Software.
+4. No Sublicensing: You may not sublicense the Software to any third party.
+5. No Public Display: You may not publicly display or make the Software available to the public.
+
+## Disclaimer of Warranty
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT, OR OTHERWISE, ARISING FROM, OUT OF, OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+## Termination
+
+Any violation of the above restrictions will result in the termination of your right to use the Software. Upon termination, you must destroy all copies of the Software in your possession.
+
+## Governing Law
+
+This License Agreement shall be governed by and construed in accordance with the laws, without regard to its conflict of law principles.
+
+---
+
+© 2024 github.com/gl0b3x . All rights reserved.
